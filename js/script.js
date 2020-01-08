77893f056e1216d5e9d32195bd42f25ae128c172
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-08 16:12:11 +0100

    Aggiunto numero posizione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-// alert('ciao')
-
-// Chiedi all’utente il cognome,
-
 var cognome = prompt('Qual\'è il tuo cognome?');
 console.log(cognome);
+
 while (cognome.length <=0){
   cognome = prompt('Qual\'è il tuo cognome?');
 }
-// inseriscilo in un array con altri cognomi,
+
 var listaCognomi = ['Dybala', 'Ronaldo', 'Bonucci', 'Chiellini', 'Buffon','Del Piero'];
 console.log(listaCognomi);
 
-
 listaCognomi.push(cognome);
-console.log(listaCognomi);
-
-// stampa la lista ordinata alfabeticamente (in html ul > li).
 listaCognomi.sort();
-console.log(listaCognomi);
-// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
 
 for (var i = 0; i < listaCognomi.length; i++) {
-  document.getElementById('squadra').innerHTML +=('<li>' + listaCognomi[i] + '</li>')
+  document.getElementById('squadra').innerHTML +=('<li>' + listaCognomi[i] + '</li>');
+  console.log(listaCognomi[i]);
 }
+console.log(listaCognomi.indexOf(cognome));
+
+var posizione = listaCognomi.indexOf(cognome);
+document.getElementById('posizione_cognome').innerHTML = posizione;
