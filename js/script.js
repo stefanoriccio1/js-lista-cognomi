164c31c07403fe70a40226cd5ad8d03eba4981d0
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-08 15:13:13 +0100

    Chiesto cognome all'utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert('ciao')
+// alert('ciao')
+
+// Chiedi all’utente il cognome,
+
+var cognome = prompt('Qual\'è il tuo cognome?');
+console.log(cognome);
+// inseriscilo in un array con altri cognomi,
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
